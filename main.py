ffa9f8170abb65b9ab6d18989a9db0ccea033a6f
Egor Boev <egorcoders@gmail.com>
2023-03-22 12:50:09 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=8000, host='0.0.0.0', reload=True)
