5027cc67dc1d83604f16283ff26593084347060a
Vladislav <v.sergienko@astondevs.ru>
2023-03-31 13:01:48 +0300

    remove logic from controller

--- endpoints/crud.py
+++ endpoints/crud.py
@@ -1,7 +1,19 @@
+from fastapi import HTTPException, status
+from schemas.schemas import TemplateForPaymentSchema
+
 from .queries import retrieve_template_query
 
 
 async def get_template_for_payment(session, template_id):
     query = retrieve_template_query(template_id)
     data = await session.execute(query)
-    return data.first()
+    template = data.first()
+
+    if template is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Template for payment with id:{template_id} not found",
+        )
+    template_model = TemplateForPaymentSchema.from_orm(template[0])
+
+    return template_model
--- endpoints/endpoints.py
+++ endpoints/endpoints.py
@@ -1,5 +1,5 @@
 from db.database import get_db
-from fastapi import APIRouter, Depends, Query, Response, status
+from fastapi import APIRouter, Depends, Query
 from schemas.schemas import TemplateForPaymentSchema
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -9,15 +9,7 @@ router = APIRouter(prefix="/api/v1", tags=["template"])
 
 
 @router.get("/get-details", status_code=200, response_model=TemplateForPaymentSchema)
-async def get_template(
-    response: Response, template_id: int = Query(), session: AsyncSession = Depends(get_db)
-):
+async def get_template(template_id: int = Query(), session: AsyncSession = Depends(get_db)):
     """Get template for payment by ID"""
     template_object = await get_template_for_payment(session, template_id)
-    if template_object is None:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return response
-
-    template = TemplateForPaymentSchema.from_orm(template_object[0])
-
-    return template
+    return template_object
