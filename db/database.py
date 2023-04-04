a462713c5d8c46b21708fb7965499ef574418639
Vladislav <v.sergienko@astondevs.ru>
2023-04-04 12:23:39 +0300

    added BaseSettings

--- db/database.py
+++ db/database.py
@@ -1,21 +1,24 @@
-from config import DB_URL
+from core.database import get_settings
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+settings = get_settings()
+
+
 engine = create_async_engine(
-    DB_URL,
-    pool_size=1,
-    max_overflow=5,
-    pool_timeout=30,
-    pool_recycle=-1,
-    pool_pre_ping=False,
+    settings.postgres.url,
+    pool_size=settings.postgres.pool_size,
+    max_overflow=settings.postgres.max_overflow,
+    pool_timeout=settings.postgres.pool_timeout,
+    pool_recycle=settings.postgres.pool_recycle,
+    pool_pre_ping=settings.postgres.pool_pre_ping,
 )
 
 async_session = async_sessionmaker(
     bind=engine,
-    autocommit=False,
-    autoflush=False,
-    expire_on_commit=False,
+    autocommit=settings.async_sessionmaker.autocommit,
+    autoflush=settings.async_sessionmaker.autoflush,
+    expire_on_commit=settings.async_sessionmaker.expire_on_commit,
     class_=AsyncSession,
 )
 
