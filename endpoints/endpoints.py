629f8b8866cd1b0c6029a09e1f29c86dc800a660
Timur Vakhitov <t.vakhitov@astondevs.ru>
2023-04-06 16:16:54 +0300

    Use pydantic

--- endpoints/endpoints.py
+++ endpoints/endpoints.py
@@ -1,6 +1,6 @@
 from db.database import get_db
-from fastapi import APIRouter, Depends, Query
-from schemas.schemas import TemplateForPaymentSchema
+from fastapi import APIRouter, Depends, Query, status
+from schemas.schemas import TemplateForExchangeRatesSchema, TemplateForPaymentSchema
 from services.crud import get_exchange_rates, get_template_for_payment
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,8 +14,8 @@ async def get_template(template_id: int = Query(), session: AsyncSession = Depen
     return template_object
 
 
-@router.get("/rates", status_code=200)
-async def get_rates(currency_from: str, currency_to: str, units: int = 100):
+@router.get("/rates", status_code=status.HTTP_200_OK, response_model=TemplateForExchangeRatesSchema)
+async def get_rates(currency_from: str, currency_to: str, units: float = 100):
     """Get exchange rates"""
     results = await get_exchange_rates(currency_from, currency_to, units)
     return results
