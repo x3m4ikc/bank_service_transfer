b6509aa3aad706a5f56b9a0c953d4a9d72415334
Timur Vakhitov <t.vakhitov@astondevs.ru>
2023-04-26 09:32:43 +0300

    psycopg2-binary

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
+    uvicorn.run("main:app", port=8002, host="0.0.0.0", reload=True)
